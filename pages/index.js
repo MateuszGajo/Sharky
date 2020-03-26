e1b1a408a74f82c4ce95b10ffca3d9daa7830e6d
Mateusz Gajo <mateusz.gajo9@wp.pl>
2020-03-26 08:22:15 +0100

    configured project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+import "./styles/global.scss";
+import "./styles/reset.scss";
 const Index = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>ddsa</h1>
+    </div>
+  );
 };
 export default Index;
