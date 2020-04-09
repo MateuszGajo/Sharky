a178400bd15212080e49ca49d54f4b4db6f37258
Mateusz Gajo <mateusz.gajo9@wp.pl>
2020-04-09 10:52:32 +0200

    start setting

--- features/routes.js
+++
@@ -1,6 +0,0 @@
-const Router = require("nextjs-dynamic-routes");
-
-const router = new Router();
-router.add({ name: "post", pattern: "/post/:postId" });
-
-module.exports = router;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-import SignIn from "./signin";
-import SignUp from "./signup";
-const Index = () => {
-  return <SignUp />;
-=======
-import Home from "./home";
-const Index = () => {
-  return <Home />;
->>>>>>> features/home
-};
 
+const Index = () => {
+  return <div></div>;
+};
 export default Index;
