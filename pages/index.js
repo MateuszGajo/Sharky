8f25d5dafe72bf58f4df60043ad5bc6e2ba5828e
Mateusz Gajo <mateusz.gajo9@wp.pl>
2020-04-09 10:53:43 +0200

    fixed merge conflict

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import SignIn from "./signin";
-import SignUp from "./signup";
-const Index = () => {
-  return <SignUp />;
-=======
 import Home from "./home";
 const Index = () => {
   return <Home />;
->>>>>>> features/home
 };
 
 export default Index;
