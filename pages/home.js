d15c13bef0ffc7ac203f0ba54514fc18297322ed
Mateusz Gajo <mateusz.gajo9@wp.pl>
2020-04-21 08:48:57 +0200

    Redesigned home section

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Router from "../features/routes";
-import { FaUserCircle } from "react-icons/fa";
 import { FiMessageCircle } from "react-icons/fi";
 import { AiOutlineShareAlt } from "react-icons/ai";
 import { IoIosHeartEmpty } from "react-icons/io";
@@ -11,20 +10,30 @@ import "./styles/main.scss";
 
 const Home = () => {
   const [postText, setPostText] = useState("");
+  const [users, setUsers] = useState({
+    234: {
+      id: 234,
+      firstName: "Janek",
+      lastName: "Kowalski",
+      photo: "profile.png",
+    },
+    453: {
+      id: 453,
+      firstName: "Janek",
+      lastName: "Kowalski",
+      photo: "profile.png",
+    },
+  });
   const [posts, setPosts] = useState([
     {
-      id: 1,
-      firstName: "Kamil",
-      lastName: "Latnikowski",
+      userId: 234,
       content: "lorem lorem ala lorem",
       comments: 12,
       likes: [123, 545, 23],
       shares: 18,
     },
     {
-      id: 1,
-      firstName: "Kamilaa",
-      lastName: "Latnikowskiaaa",
+      userId: 453,
       content: "lorem lorem ala lorem dasd",
       comments: 122,
       likes: [543, 563, 232],
@@ -34,10 +43,12 @@ const Home = () => {
   return (
     <HomeLayout>
       <MessageBox btnSize="small" value={postText} onChange={setPostText} />
-      {posts.map((post) => {
+      {posts.map((post, index) => {
         let isLiked = post.likes.indexOf(123) !== -1;
+        const user = users[post.userId];
         return (
           <div
+            key={index}
             className="home-wrapper__main__content__post"
             onClick={() => {
               Router.pushRoute("post", { postId: post.id });
@@ -45,12 +56,16 @@ const Home = () => {
           >
             <div className="home-wrapper__main__content__post_body">
               <div className="home-wrapper__main__content__post_body__picture">
-                <FaUserCircle />
+                <img
+                  src={"/static/images/" + user.photo}
+                  alt=""
+                  className="home-wrapper__main__content__post_body__picture--img"
+                />
               </div>
               <div className="home-wrapper__main__content__post_body__text">
                 <div className="home-wrapper__main__content__post_body__text__author">
                   <span className="home-wrapper__main__content__post_body__text__author--span">
-                    {post.firstName} {post.lastName}
+                    {user.firstName} {user.lastName}
                   </span>
                 </div>
                 <div className="home-wrapper__main__content__post_body__text__content">
@@ -75,7 +90,7 @@ const Home = () => {
                 onClick={(e) => {
                   e.stopPropagation();
                   if (!isLiked) {
-                    console.log("zapisuje do bazy");
+                    //save db
                   }
                 }}
               >
