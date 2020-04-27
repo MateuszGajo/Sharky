1ef121407832c27595c0ea4eb8519485d3c55c81
Mateusz <mateusz.gajo9@wp.pl>
2020-04-27 08:39:56 +0200

    clear index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./styles/main.scss";
-import PostList from "../features/components/Lists/PostList/PostList";
-import DownBarButtons from "../features/common/DownBarButtons/DownBarButtons";
-import About from "../features/components/About/About";
 
 const Index = () => {
-  return <About />;
+  return null;
 };
 export default Index;
