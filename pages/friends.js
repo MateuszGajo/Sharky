f83c540bb2f4ad48643ca82382691948a7bd2797
Mateusz <mateusz.gajo9@wp.pl>
2020-04-28 12:40:36 +0200

    Used reuseable component on friends page

--- pages/friends.js
+++ pages/friends.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import cx from "classnames";
-import Router from "../features/routes";
+import FriendList from "../features/components/Lists/FriendsList/FriendList";
 
-const Frineds = () => {
+const Friends = () => {
   const [users, setUsers] = useState({
     234: {
       id: 234,
       firstName: "Janek",
       lastName: "Kowalski",
+      photo: "profile.png",
       online: false,
       amonutsOfFriends: 20,
     },
@@ -16,6 +16,7 @@ const Frineds = () => {
       firstName: "Janek",
       lastName: "Kowalski",
       online: false,
+      photo: "profile.png",
       amonutsOfFriends: 20,
     },
     156: {
@@ -23,132 +24,30 @@ const Frineds = () => {
       firstName: "Janek",
       lastName: "Kowalski",
       online: false,
+      photo: "profile.png",
       amonutsOfFriends: 20,
     },
   });
   const friendLists = [
     {
       userId: 234,
-      relationShip: "Rodzina",
+      relationShip: "rodzina",
     },
     {
       userId: 453,
-      relationShip: "Przyjaciel",
+      relationShip: "przyjaciel",
     },
     {
       userId: 156,
-      relationShip: "Znajomy",
+      relationShip: "znajomy",
     },
   ];
 
   return (
     <div className="home-wrapper__main__content__friends">
-      {friendLists.map((friend) => {
-        const user = users[friend.userId];
-        return (
-          <div
-            className="home-wrapper__main__content__friends__item"
-            key={user.id}
-          >
-            <div className="home-wrapper__main__content__friends__item--picture">
-              <img
-                src="/static/images/profile.png"
-                className="home-wrapper__main__content__friends__item--picture--img"
-                onClick={() => {
-                  Router.pushRoute("profile", { id: user.id });
-                }}
-              />
-            </div>
-            <div className="home-wrapper__main__content__friends__item__info">
-              <div className="home-wrapper__main__content__friends__item__info__first-column">
-                <div className="home-wrapper__main__content__friends__item__info__first-column--name">
-                  <span
-                    className="home-wrapper__main__content__friends__item__info__first-column--name--span"
-                    onClick={() => {
-                      Router.pushRoute("profile", { id: user.id });
-                    }}
-                  >
-                    {user.firstName} {user.lastName}
-                  </span>
-                </div>
-                <div className="home-wrapper__main__content__friends__item__info__first-column--amounts-of-friends">
-                  <span className="home-wrapper__main__content__friends__item__info__first-column--amounts-of-friends--span">
-                    Liczba znajomych: {user.amonutsOfFriends}
-                  </span>
-                </div>
-              </div>
-              <div className="home-wrapper__main__content__friends__item__info__second-column">
-                <div className="home-wrapper__main__content__friends__item--status">
-                  <div
-                    className={cx(
-                      "home-wrapper__main__content__friends__item--status--circle",
-                      {
-                        "primary-background": friend.relationShip === "Znajomy",
-                        "family-background": friend.relationShip === "Rodzina",
-                        "pal-background": friend.relationShip === "Przyjaciel",
-                      }
-                    )}
-                  >
-                    <span>{friend.relationShip}</span>
-                  </div>
-                  <div className="home-wrapper__main__content__friends__item--status__change-status">
-                    <div
-                      className={cx(
-                        "home-wrapper__main__content__friends__item--status--circle primary-background",
-                        {
-                          "brightness-reduce hover-brightness":
-                            friend.relationShip !== "Znajomy",
-                        }
-                      )}
-                      onClick={() => {
-                        if (friend.relationShip !== "Znajomy") {
-                          //save to db
-                        }
-                      }}
-                    >
-                      <span>Znajomy</span>
-                    </div>
-                    <div
-                      className={cx(
-                        "home-wrapper__main__content__friends__item--status--circle pal-background ",
-                        {
-                          "brightness-reduce hover-brightness":
-                            friend.relationShip !== "Przyjaciel",
-                        }
-                      )}
-                      onClick={() => {
-                        if (friend.relationShip !== "Przyjaciel") {
-                          //save to db
-                        }
-                      }}
-                    >
-                      <span>Przyjaciel</span>
-                    </div>
-                    <div
-                      className={cx(
-                        "home-wrapper__main__content__friends__item--status--circle family-background ",
-                        {
-                          "brightness-reduce hover-brightness":
-                            friend.relationShip !== "Rodzina",
-                        }
-                      )}
-                      onClick={() => {
-                        if (friend.relationShipd !== "Rodzina") {
-                          //save to db
-                        }
-                      }}
-                    >
-                      <span>Rodzina</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      <FriendList listOfFriends={friendLists} users={users} />
     </div>
   );
 };
 
-export default Frineds;
+export default Friends;
