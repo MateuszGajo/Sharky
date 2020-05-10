56f5fd661284b6b28793afa09b5078a9d376892f
Mateusz <mateusz.gajo9@wp.pl>
2020-05-10 08:44:59 +0200

    Changed path to file

--- pages/fanpages.js
+++ pages/fanpages.js
@@ -1,13 +1,13 @@
 import React from "react";
 import HomeLayout from "../features/components/Layout/Home/HomeLayout";
-import FanpageList from "../features/components/Lists/FanpageList/FanpageList";
+import ListOfFanpages from "../features/components/Lists/Fanpages/Fanpages";
 import "./styles/main.scss";
 
 const Fanpages = () => {
   return (
     <HomeLayout>
       <div className="home-wrapper__main__content__fanpages">
-        <FanpageList />
+        <ListOfFanpages />
       </div>
     </HomeLayout>
   );
