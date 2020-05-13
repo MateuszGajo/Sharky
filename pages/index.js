c077c049d18705224692fcfa305b5bdda8f352fd
Mateusz <mateusz.gajo9@wp.pl>
2020-05-13 14:11:47 +0200

    Adde RWD on photo,card section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./styles/main.scss";
+import Card from "../features/components/Lists/Card/Card";
+import Photos from "../features/components/Lists/Photos/Photos";
+import Post from "../features/components/Post/Post";
 
 const Index = () => {
-  return null;
+  return (
+    <>
+      <Post />
+    </>
+  );
 };
 export default Index;
