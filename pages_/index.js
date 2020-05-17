a37efe0d257bfdd2e56b02ffd1f2096966df5546
Mateusz <mateusz.gajo9@wp.pl>
2020-05-17 19:16:16 +0200

    improved authentication text

--- pages_/index.js
+++ pages_/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles/main.scss";
+import Authentication from "../features/components/Layout/Authentication/Authentication";
 
 const Index = () => {
-  return null;
+  return <Authentication type="signin" />;
 };
 export default Index;
