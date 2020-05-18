7f078ca3b4c3d06b9968b7c40530d98939a11f18
Mateusz <mateusz.gajo9@wp.pl>
2020-05-18 07:38:37 +0200

    improved authentication layout

--- pages_/index.js
+++ pages_/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../styles/main.scss";
-import Authentication from "../features/components/Layout/Authentication/Authentication";
 
 const Index = () => {
-  return <Authentication type="signin" />;
+  return null;
 };
 export default Index;
