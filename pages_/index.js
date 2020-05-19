357fcad2b257810ba80bf1353026823857b44f69
Mateusz <mateusz.gajo9@wp.pl>
2020-05-19 07:45:27 +0200

    Added margin between posts, improved cotainer card component

--- pages_/index.js
+++ pages_/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "../styles/main.scss";
+import HomeLayout from "../features/components/Layout/Home/HomeLayout";
+import Fanpages from "../features/components/Lists/Fanpages/Fanpages";
+import Groups from "../features/components/Lists/Groups/Groups";
+import People from "../features/components/Lists/People/People";
+import Posts from "../features/components/Lists/Posts/Posts";
 
 const Index = () => {
-  return null;
+  return (
+    <HomeLayout>
+      <Posts />
+    </HomeLayout>
+  );
 };
 export default Index;
