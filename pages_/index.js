fdcb2223ff7df913bab465012aa00c3e0bc9b5be
Mateusz <mateusz.gajo9@wp.pl>
2020-05-20 08:26:23 +0200

    improved card component

--- pages_/index.js
+++ pages_/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import "../styles/main.scss";
-import HomeLayout from "../features/components/Layout/Home/HomeLayout";
-import Fanpages from "../features/components/Lists/Fanpages/Fanpages";
-import Groups from "../features/components/Lists/Groups/Groups";
-import People from "../features/components/Lists/People/People";
-import Posts from "../features/components/Lists/Posts/Posts";
 
 const Index = () => {
-  return (
-    <HomeLayout>
-      <Posts />
-    </HomeLayout>
-  );
+  return null;
 };
 export default Index;
