062621d4140ca8eea41e76cc42b226f025a96aa4
Mateusz <mateusz.gajo9@wp.pl>
2020-05-21 08:51:26 +0200

    improved card component, added new popup 'invite person'

--- pages_/index.js
+++ pages_/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "../styles/main.scss";
+import InvitePerson from "../features/common/PopUp/InvitePerson/InvitePerson";
 
 const Index = () => {
-  return null;
+  return (
+    <>
+      <InvitePerson />
+      <div>dasdasdas</div>
+      <div>sadasd</div>
+      <div>fsdfds</div>
+      <div>fdasdsa</div>
+    </>
+  );
 };
 export default Index;
