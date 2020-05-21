fa21e3ed675c76237bdb68f48903a31adc816db4
Mateusz <mateusz.gajo9@wp.pl>
2020-05-21 09:47:41 +0200

    cleared index

--- pages_/index.js
+++ pages_/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import "../styles/main.scss";
-import InvitePerson from "../features/common/PopUp/InvitePerson/InvitePerson";
 
 const Index = () => {
-  return (
-    <>
-      <InvitePerson />
-      <div>dasdasdas</div>
-      <div>sadasd</div>
-      <div>fsdfds</div>
-      <div>fdasdsa</div>
-    </>
-  );
+  return null;
 };
 export default Index;
