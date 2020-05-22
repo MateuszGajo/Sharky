ab0f1f3449a3ff43cce047afc9d6f573a8770e88
Mateusz <mateusz.gajo9@wp.pl>
2020-05-22 08:41:57 +0200

    Improved RWD

--- pages_/index.js
+++ pages_/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "../styles/main.scss";
+import Posts from "../features/components/Lists/Posts/Posts";
+import Messenger from "../features/components/Messenger/Messenger";
 
 const Index = () => {
-  return null;
+  return <Messenger />;
 };
 export default Index;
