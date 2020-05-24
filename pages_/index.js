a937c1bb27b36b58d87f9846646cb4c056d2419e
Mateusz <mateusz.gajo9@wp.pl>
2020-05-24 11:22:40 +0200

    fixed merge conflict

--- pages_/index.js
+++ pages_/index.js
@@ -1,14 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import Profile from "./profile";
 
 const Index = () => {
   return <Profile />;
-=======
-import "../styles/main.scss";
-
-const Index = () => {
-  return null;
->>>>>>> common-layout
 };
-export default Index;
