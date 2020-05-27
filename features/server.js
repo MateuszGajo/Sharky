04727664db63a6cdd9640e2f3d2ddb715f03d704
Mateusz <mateusz.gajo9@wp.pl>
2020-05-27 21:17:26 +0200

    Changed node middleware

---
+++ features/server.js
@@ -0,0 +1,14 @@
+import koa from "koa";
+import next from "next";
+import Router from "./routes";
+
+const app = next({ dev: process.env.NODE_ENV !== "production" });
+const handle = Router.getRequestHandler(app);
+const server = koa();
+
+(async () => {
+  await app.prepare();
+
+  server.get("*", (req, res) => handle(req, res));
+  server.listen(3000);
+})();
