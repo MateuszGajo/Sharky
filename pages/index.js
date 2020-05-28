31fd071bd63f341cb222270bdb36ef9395cd2a34
Mateusz <mateusz.gajo9@wp.pl>
2020-05-28 22:26:58 +0200

    fixed merge conflict

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@
 import I18nProvider from 'next-translate/I18nProvider'
 import React from 'react'
 import C from '../pages_'
-import ns0 from '../locales/en/common.json'
-import ns1 from '../locales/en/component.json'
+import ns0 from '../public/locales/en/common.json'
+import ns1 from '../public/locales/en/component.json'
 
 const namespaces = { 'common': ns0, 'component': ns1 }
 
