4733a0362e4636d1008764931e2906b45abb2e13
Mateusz <mateusz.gajo9@wp.pl>
2020-09-02 10:11:19 +0200

    clear index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,7 @@
-import React, { useState, useEffect, useContext } from "react";
-import Router from "next/router";
-import HomeLayout from "@components/Layout/Home/HomeLayout";
-import MessageBox from "@common/MessageBox/MessageBox";
-import Spinner from "@components/Spinner/Spinner";
-import Posts from "@components/Lists/Posts/Posts";
-import AppContext from "@features/context/AppContext";
-import { getOwner } from "@features/service/Functions/index";
-import People from "@components/Lists/People/People";
-import "../styles/main.scss";
+import React from "react";
 
 const Index = () => {
-  const { setOwner, isAuth, setStatusOfAuth } = useContext(AppContext);
-
-  useEffect(() => {
-    getOwner({ setStatusOfAuth, setOwner });
-  }, []);
-
-  if (isAuth == null) return <Spinner />;
-  else if (!isAuth) {
-    Router.push("/signin");
-    return <Spinner />;
-  }
-
-  return (
-    <HomeLayout>
-      <People idUser={1} />
-    </HomeLayout>
-  );
+  return <div></div>;
 };
 
 export default Index;
