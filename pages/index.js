d9f1a82a051c8610fb35aa7eb75cd80a5f92542f
Mateusz <mateusz.gajo9@wp.pl>
2020-09-04 10:06:23 +0200

    Added seo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import Signin from "./signin";
+import i18next from "@i18n";
+const { useTranslation } = i18next;
 
 const Index = () => {
-  return <div></div>;
+  const { t } = useTranslation(["index"]);
+
+  const description = t("index:description");
+  const keyWords = t("index:key-words");
+  return (
+    <>
+      <Head>
+        <meta name="description" content={description} />
+        <meta name="author" content="Mateusz Gajo" />
+        <meta name="keywords" content={keyWords} />
+        <meta name="url" content="https://sharkyapp.herokuapp.com/" />
+        <meta property="og:title" content="Sharky" />
+        <meta property="og:description" content={description} />
+      </Head>
+      <Signin />
+    </>
+  );
 };
 
 export default Index;
