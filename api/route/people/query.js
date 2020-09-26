a637dd11c4643ae9388094ce6eee81b12a9502eb
Mateusz <mateusz.gajo9@wp.pl>
2020-09-26 09:52:37 +0200

    subquery name improved

--- api/route/people/query.js
+++ api/route/people/query.js
@@ -1,35 +1,35 @@
 const group_getFriendsQuery = `
-with userFriends as(
+with friendIds as(
 select user_id_1 as "userId" from friends where user_id_2=$1  and status='1' 
 union
 select user_id_2 as "userId" from friends where  user_id_1=$1  and status='1'  
 ),
         
-userFriendsCounted as(
+friendsCounted as(
 select a."userId", sum(a.count) as "numberOfFriends"	  
-from( select user_id_1 as "userId",count(user_id_1)  from friends where user_id_1 in(select * from userFriends) group by user_id_1
+from( select user_id_1 as "userId",count(user_id_1)  from friends where user_id_1 in(select * from friendIds) group by user_id_1
 union
-select user_id_2 as "userId",count(user_id_2)  from friends where user_id_2 in(select * from userFriends) group by user_id_2) as a
+select user_id_2 as "userId",count(user_id_2)  from friends where user_id_2 in(select * from friendIds) group by user_id_2) as a
 group by a."userId"
 ),
         
-invitedFriends as (
-select user_id as "userId",id::text as "subId",status::text, CASE WHEN status='0'  THEN true::text  else null end as "isInvitationSent" from group_users where user_id in (select * from userFriends) and group_id=$2	
+friendsInvited as (
+select user_id as "userId",id::text as "subId",status::text, CASE WHEN status='0'  THEN true::text  else null end as "isInvitationSent" from group_users where user_id in (select * from friendIds) and group_id=$2	
 ),
 		
-notInvitedFriends as(
-select *, null as "subId", null as status, null as "isInvitationSent" from userFriends where "userId" not in (select "userId" from invitedFriends)
+friendsNotInvited as(
+select *, null as "subId", null as status, null as "isInvitationSent" from friendIds where "userId" not in (select "userId" from friendsInvited)
 ),
 		
 friendsStatus as (
-select * from invitedFriends where status !='1'
+select * from friendsInvited where status !='1'
 union
-select * from notInvitedFriends
+select * from friendsNotInvited
 )
 		
 select a.*, b."numberOfFriends", c.first_name as "firstName", c.last_name as "lastName", c.photo
 from friendsStatus as a
-inner join userFriendsCounted as b on a."userId" = b."userId"
+inner join friendsCounted as b on a."userId" = b."userId"
 inner join users as c on a."userId" = c.id
 order by c.first_name asc
 limit 21 offset $3`;
@@ -49,18 +49,18 @@ select user_id_2 as "userId", count(user_id_2)  from friends where user_id_2 in(
 group by a."userId"
 ),
         
-invitedUsers as (
+usersInvited as (
 select  user_id as  "userId",id::text as "subId",status::text, CASE WHEN status='0'  THEN true::text  else null end as "isInvitationSent" from group_users where user_id in (select *from usersSorted) and group_id=$3
 ),
     
-notInvitedUsers as(
-select id as "userId", null as "subId", null as status, null as "isInvitationSent" from usersSorted where id not in (select "userId" from invitedUsers)
+usersNotInvited as(
+select id as "userId", null as "subId", null as status, null as "isInvitationSent" from usersSorted where id not in (select "userId" from usersInvited)
 ),
     
 usersStatus as (
-select * from invitedUsers where status !='1'
+select * from usersInvited where status !='1'
 union
-select * from notInvitedUsers
+select * from usersNotInvited
 )
     
     
