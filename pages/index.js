2a5f3dfed11e0cf5aae7f2daab1c04710f8623eb
Mateusz <mateusz.gajo9@wp.pl>
2020-09-26 07:44:43 +0200

    clear index, main.scss

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,7 @@
-import React, { useState, useEffect, useContext } from "react";
-import Router from "next/router";
-import Spinner from "@components/Spinner/Spinner";
-import AppContext from "@features/context/AppContext";
-import { getOwner } from "@features/service/Functions/index";
-import HomeLayout from "@components/Layout/Home/HomeLayout";
-import Fanpages from "@components/Lists/Fanpages/Fanpages";
-import People from "@components/Lists/People/People";
-import Posts from "@components/Lists/Posts/Posts";
-import Report from "@common/PopUp/Report/Report";
-import "../styles/main.scss";
+import React from "react";
 
 const Index = () => {
-  const { setOwner } = useContext(AppContext);
-  const [isAuth, setStatusOfAuth] = useState(null);
-
-  useEffect(() => {
-    getOwner({ setStatusOfAuth, setOwner });
-  }, []);
-
-  if (isAuth == null) return <Spinner />;
-  else if (!isAuth) {
-    Router.push("/signin");
-    return <Spinner />;
-  }
-
-  return (
-    <>
-      <HomeLayout>
-        <Fanpages userId={1} />
-        <People userId={1} />
-        <Posts userId={1} />
-        {/* <Report /> */}
-      </HomeLayout>
-    </>
-  );
+  return <div></div>;
 };
 
 export default Index;
