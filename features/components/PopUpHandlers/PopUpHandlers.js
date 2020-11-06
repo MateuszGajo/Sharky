a7fb3b5e9c0eeaf01708dd2cf794593d7da33237
Mateusz <mateusz.gajo9@wp.pl>
2020-11-06 10:17:53 +0100

    created: pop up handlers component

---
+++ features/components/PopUpHandlers/PopUpHandlers.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import Error from "@common/PopUp/Error/Error";
+import Prompt from "@common/PopUp/Prompt/Prompt";
+import AppContext from "@features/context/AppContext";
+
+const PopUpHandlers = () => {
+  const { isPrompt, isError } = useContext(AppContext);
+  return (
+    <>
+      {isError && <Error message={isError} />}
+      {isPrompt && <Prompt message={isPrompt} />}
+    </>
+  );
+};
+
+return PopUpHandlers;
