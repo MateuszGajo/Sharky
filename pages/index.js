bf1d62125fb749da54f70956f11e5e1b378cfe2b
Mateusz <mateusz.gajo9@wp.pl>
2020-11-10 09:53:57 +0100

    cleared: index and main.scss

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
-import React, { useState, useEffect, useContext } from "react";
-import Router from "next/router";
-import Spinner from "@components/Spinner/Spinner";
-import AppContext from "@features/context/AppContext";
-import { getOwner } from "@features/service/Functions/index";
-import Posts from "@components/Lists/Posts/Posts";
-import PopUpHandlers from "@components/PopUpHandlers/PopUpHandlers";
-import HomeLayout from "@components/Layout/Home/HomeLayout";
-import "../styles/main.scss";
+import React from "react";
 
 const Index = () => {
-  const { setOwner } = useContext(AppContext);
-  const [isAuth, setStatusOfAuth] = useState(null);
-
-  useEffect(() => {
-    getOwner({ setStatusOfAuth, setOwner });
-  }, []);
-
-  if (isAuth == null) return <Spinner />;
-  else if (!isAuth) {
-    Router.push("/signin");
-    return <Spinner />;
-  }
-
-  return (
-    <>
-      <HomeLayout>
-        <PopUpHandlers />
-        <Posts />
-      </HomeLayout>
-    </>
-  );
+  return <div></div>;
 };
 
 export default Index;
